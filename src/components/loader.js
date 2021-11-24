ecdce86ee6e0a87d74a832216cad4d8cf4f2e3d8
Radostin Dimitrov <radostin1234567891122@abv.bg>
2021-11-24 14:47:18 +0200

    delete on loader page

--- src/components/loader.js
+++
@@ -1,9 +0,0 @@
-function Loader() {
-    return (
-        <div className="loader_bg">
-            <div className="loader"><img src="images/loading.gif" alt="#" /></div>
-        </div>
-    )
-}
-
-export default Loader
