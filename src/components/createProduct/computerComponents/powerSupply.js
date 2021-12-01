7a4176687b2c2d8b1f0f5997da923b73e31d0ae8
Radostin Dimitrov <radostin1234567891122@abv.bg>
2021-12-01 14:14:04 +0200

    add power supplay skelet

--- src/components/createProduct/computerComponents/powerSupply.js
+++ src/components/createProduct/computerComponents/powerSupply.js
@@ -11,57 +11,21 @@ function PowerSupply() {
                     <form action="#">
                         <div className="details-box">
                             <div className="input-box">
-                                <span className="details">Brand</span>
-                                <select>
-                                    <option>Desktop.bg</option>
-                                    <option>Asus</option>
-                                    <option>Acer</option>
-                                    <option>Lenovo</option>
-                                    <option>Dell</option>
-                                    <option>Apple</option>
-                                    <option>Fujitsu</option>
-                                    <option>Huawei</option>
-                                    <option>G:RIGS</option>
-                                    <option>Other</option>
-                                </select>
-                            </div>
-                            <div className="input-box">
                                 <span className="details">Names</span>
                                 <input type="text" id="name" name="name" placeholder="Enter name of brand" />
                                 <input type="text" id="model" name="model" placeholder="Enter name of model" />
                             </div>
                             <div className="input-box">
-                                <span className="details">Suitable for?</span>
-                                <input type="text" id="suitable" name="suitable" placeholder="Enter info for suitable" />
+                                <span className="details">Power</span>
+                                <input type="text" id="power" name="power" placeholder="Enter info for power" />
                             </div>
                             <div className="input-box">
-                                <span className="details">Hardware</span>
-                                <input type="text" id="motherboard" name="motherboard" placeholder="Enter motherboard model and brand" required />
-                                <input type="text" id="CPU" name="CPU" placeholder="Enter CPU brand and model" required />
-                                <input type="text" id="RAM" name="RAM" placeholder="Enter info for RAM" required />
-                                <label>External video card?</label>
-                                <select>
-                                    <option>Yes</option>
-                                    <option>No</option>
-                                </select>
-                                <input type="text" id="external-video-card" name="external-video-card" placeholder="Enter info for external video card" />
-                                <label>HDD?</label>
-                                <select>
-                                    <option>Yes</option>
-                                    <option>No</option>
-                                </select>
-                                <input type="text" id="HDD" name="HDD" placeholder="Enter info for HDD" />
-                                <label>SSD?</label>
-                                <select>
-                                    <option>Yes</option>
-                                    <option>No</option>
-                                </select>
-                                <input type="text" id="SSD" name="SSD" placeholder="Enter info for SSD" />
-                                <input type="text" id="power-supply" name="power-supply" placeholder="Enter brand, model, cert, watts on PSU"/>
+                                <span className="details">Size</span>
+                                <input type="text" id="size" name="size" placeholder="Enter info for size and type" required />
                             </div>
                             <div className="input-box">
-                                <span className="details">Interface</span>
-                                <input type="text" id="interface" name="interface" placeholder="Enter info for the interface" required />
+                                <span className="details">Certificate</span>
+                                <input type="text" id="interface" name="interface" placeholder="Enter info for certificate" required />
                             </div>
                             <div className="input-box">
                                 <span className="details">Description</span>
