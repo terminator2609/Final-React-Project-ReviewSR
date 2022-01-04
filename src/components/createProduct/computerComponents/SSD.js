339b40759dc7b625c4e4327c9cc5ed08f3cf0d0b
Radostin Dimitrov <radostin1234567891122@abv.bg>
2022-01-05 01:00:01 +0200

    add create SSD page functions

--- src/components/createProduct/computerComponents/SSD.js
+++ src/components/createProduct/computerComponents/SSD.js
@@ -1,6 +1,30 @@
 import CreateProduct from "../createProduct"
+import SSDService from "../../../services/SSD"
+import { useHistory } from "react-router-dom"
 
 function SSD() {
+
+    const history = useHistory()
+
+    const desHandler = (e) => {
+
+        if (e.target.parentNode.children[2].children[0].style.display === "block") {
+            e.target.parentNode.children[2].children[0].style.display = "none"
+        } else {
+            e.target.parentNode.children[2].children[0].style.display = "block"
+        }
+    }
+
+    const SSDHandler = (e) => {
+        e.preventDefault()
+
+        const formData = Object.fromEntries(new FormData(e.currentTarget))
+
+        SSDService(formData)
+
+        history.push("/")
+    }
+
 
     return (
         <div>
@@ -8,17 +32,15 @@ function SSD() {
             <div className="container-create-mobile-device">
                 <div className="title">Create SSD</div>
                 <div className="content">
-                    <form action="#">
+                    <form action="#" onSubmit={SSDHandler}>
                         <div className="details-box">
                             <div className="input-box">
                                 <span className="details">Type</span>
-                                <select>
+                                <select name="type">
                                     <option>SATA</option>
                                     <option>M.2 NVMe</option>
                                     <option>M.2 SATA</option>
-                                    <option>Other</option>
                                 </select>
-                                 <input type="text" id="other-type" name="other-type" placeholder="Enter info for other type" />
                             </div>
                             <div className="input-box">
                                 <span className="details">Names</span>
@@ -27,47 +49,51 @@ function SSD() {
                             </div>
                             <div className="input-box">
                                 <span className="details">Speeds</span>
-                                <input type="text" id="speed-read" name="speed-read" placeholder="Enter info for speed on read" required />
-                                <input type="text" id="speed-record" name="speed-record" placeholder="Enter info for speed on record" required />
+                                <input type="text" id="speed-read" name="speedRead" placeholder="Enter info for speed on read" required />
+                                <input type="text" id="speed-record" name="speedWrite" placeholder="Enter info for speed on write" required />
                             </div>
                             <div className="input-box">
                                 <span className="details">Life</span>
-                                <input type="text" id="interface" name="interface" placeholder="Enter info for the life" required />
+                                <input type="text" id="interface" name="life" placeholder="Enter info for the life" required />
                             </div>
                             <div className="input-box">
                                 <span className="details">Description</span>
-                                <input type="text" id="Other-Info" name="Other-Info" placeholder="Enter other info" required />
+                                <input type="text" id="Other-Info" name="otherInfo" placeholder="Enter other info" required />
                                 <input type="text" id="impression" name="impression" placeholder="Enter your impression" required />
                             </div>
                         </div>
 
                         <div className="gender-details">
-                            <input type="checkbox" name="desktop.bg" id="dot-1" />
-                            <input type="checkbox" name="ozone.bg" id="dot-2" />
-                            <input type="checkbox" name="gplay.bg" id="dot-3" />
-                            <input type="checkbox" name="ardes.bg" id="dot-4" />
 
                             <span className="gender-title">Partners</span>
                             <div className="category">
                                 <label htmlFor="dot-1">
-                                    <span className="dot one"></span>
+                                    <input type="checkbox" id="dot-1" onClick={desHandler} />
                                     <span className="gender">Desktop.bg</span>
-                                    <input type="text" name="link" id="link" placeholder="Enter link to product on site on my partner" style={{ display: "none" }} />
+                                    <div className="input-box">
+                                        <input type="text" className="linkDesktop" name="linkDesktop" id="link" placeholder="Enter link" style={{ display: "none" }} />
+                                    </div>
                                 </label>
                                 <label htmlFor="dot-2">
-                                    <span className="dot two"></span>
+                                    <input type="checkbox" id="dot-2" onClick={desHandler} />
                                     <span className="gender">Ozone.bg</span>
-                                    <input type="text" name="link" id="link" placeholder="Enter link to product on site on my partner" style={{ display: "none" }} />
+                                    <div className="input-box">
+                                        <input type="text" className="linkOzone" name="linkOzone" id="linkOzone" placeholder="Enter link" style={{ display: "none" }} />
+                                    </div>
                                 </label>
                                 <label htmlFor="dot-3">
-                                    <span className="dot three"></span>
+                                    <input type="checkbox" id="dot-3" onClick={desHandler} />
                                     <span className="gender">Gplay.bg</span>
-                                    <input type="text" name="link" id="link" placeholder="Enter link to product on site on my partner" style={{ display: "none" }} />
+                                    <div className="input-box">
+                                        <input type="text" className="linkGplay" name="linkGplay" id="linkGplay" placeholder="Enter link" style={{ display: "none" }} />
+                                    </div>
                                 </label>
                                 <label htmlFor="dot-4">
-                                    <span className="dot four"></span>
+                                    <input type="checkbox" id="dot-4" onClick={desHandler} />
                                     <span className="gender">Ardes.bg</span>
-                                    <input type="text" name="link" id="link" placeholder="Enter link to product on site on my partner" style={{ display: "none" }} />
+                                    <div className="input-box">
+                                        <input type="text" className="linkArdes" name="linkArdes" id="linkArdes" placeholder="Enter link" style={{ display: "none" }} />
+                                    </div>
                                 </label>
                             </div>
                         </div>
